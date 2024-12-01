e385b33fecb651b153f0b93bb467032cdb982de0
LeeYuanXu <1712306800@qq.com>
2024-12-01 22:33:19 +0800

    Update Linear.py

--- models/Linear.py
+++ models/Linear.py
@@ -1,28 +1,12 @@
 import torch
 import torch.nn as nn
 
-
 class Model(nn.Module):
     ''' Linear Model '''
     def __init__(self, args):
         super(Model, self).__init__()
 
-        self.linear = nn.Linear(args.in_dim, args.out_dim)
+        self.Linear = nn.Linear(args.d_in, args.d_out)
 
     def forward(self, x):
-        return self.linear(x)
-
-
-if __name__ == '__main__':
-    class args(object):
-        in_dim = 5
-        out_dim = 3
-
-    args = args()
-    model = Model(args)
-    print(model.parameters)
-
-    a = torch.randn(2, 5)
-    b = model(a).detach()
-    print(a)
-    print(b)
+        return self.Linear(x)
