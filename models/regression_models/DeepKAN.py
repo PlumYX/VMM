2a0cfb3902c635ece6da05b58d61a634c7c48f8e
LeeYuanXu <1712306800@qq.com>
2024-12-03 11:00:40 +0800

    Create DeepKAN.py

---
+++ models/regression_models/DeepKAN.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+from layers.KANLayer import KANLayer  # type: ignore
+
+class Monolayer_KAN(nn.Module):
+    def __init__(self, d_in, d_out, layer_norm=True):
+        super(Monolayer_KAN, self).__init__()
+
+        self.KanLayer = KANLayer(d_in, d_out)
+        self.LayerNorm = nn.LayerNorm(d_out) if layer_norm else None
+
+    def forward(self, x):
+        x, _, _, _ = self.KanLayer(x)
+        x = self.LayerNorm(x) if self.LayerNorm else x
+        return x
+
+
+class Model(nn.Module):
+    def __init__(self, configs):
+        super(Model, self).__init__()
+
+        self.e_layers = configs.e_layers
+        self.embedding_mapping = Monolayer_KAN(
+            configs.d_in, configs.d_model, configs.layer_norm)
+        self.kan_blacks = nn.ModuleList([
+            Monolayer_KAN(
+                configs.d_model, configs.d_model, configs.layer_norm
+                ) for _ in range(configs.e_layers-1)
+            ]) if configs.e_layers != 1 else None
+        self.projection = nn.Linear(configs.d_model, configs.d_out)
+
+    def forward(self, x):
+        x = self.embedding_mapping(x)
+        if self.e_layers != 1: 
+            for black in self.kan_blacks:
+                x = black(x)
+        x = self.projection(x)
+        return x
